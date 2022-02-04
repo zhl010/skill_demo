1418e9fa6c3721a5063786089f2a4180e2521c4c
Zhizhong Li <zhl010@ucsd.edu>
2022-02-04 14:11:46 -0800

    test sqr which will fail

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,5 @@
-
+public class SkillDemo{
+    public static int sqr(int m){
+        return m*m;
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void testSqr(){
+        assertEquals(10,SkillDemo.sqr(3));
+    }
+}
